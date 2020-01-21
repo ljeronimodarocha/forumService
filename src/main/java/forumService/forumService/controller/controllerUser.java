f92e7e98e95e454db5c2a308be196b03f4e3cff1
Familia Rocha <ljeronimodarocha@gmail.com>
2020-01-20 23:33:02 -0300

    restfull input and get

--- src/main/java/forumService/forumService/controller/controllerUser.java
+++ src/main/java/forumService/forumService/controller/controllerUser.java
@@ -1,22 +1,30 @@
 package forumService.forumService.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import forumService.forumService.models.user;
 import forumService.forumService.repository.userRepository;
 
 @RestController
-@RequestMapping(name = "userService")
 public class controllerUser {
 	@Autowired
 	userRepository repo;
 
-	@PutMapping(name = "/save")
+	@RequestMapping(method = RequestMethod.POST, value = "save")
 	public void save(@RequestBody user u) {
+		System.out.println("teste save");
 		repo.save(u);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "get")
+	public List<user> get() {
+		System.out.println("teste");
+		return repo.findAll();
 	}
 }
