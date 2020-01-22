daa427ed545ee508d61970b32c0a809be22fc951
Familia Rocha <ljeronimodarocha@gmail.com>
2020-01-21 23:06:40 -0300

    .

--- src/main/java/forumService/forumService/controller/controllerUser.java
+++ src/main/java/forumService/forumService/controller/controllerUser.java
@@ -3,6 +3,7 @@ package forumService.forumService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import forumService.forumService.models.user;
 import forumService.forumService.repository.userRepository;
 
 @RestController
+@CrossOrigin
 public class controllerUser {
 	@Autowired
 	userRepository repo;
