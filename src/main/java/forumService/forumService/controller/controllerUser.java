4b163eaeed46a5cb37472c8ef71725f8b878f0be
ljeronimodarocha <ljeronimodarocha@gmail.com>
2020-01-27 22:58:52 -0300

    method checkEmail with get

--- src/main/java/forumService/forumService/controller/controllerUser.java
+++ src/main/java/forumService/forumService/controller/controllerUser.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,4 +31,14 @@ public class controllerUser {
 		System.out.println("teste");
 		return repo.findAll();
 	}
+
+	@GetMapping(path = "/checkEmail/{email}")
+	public user checkEmail(@PathVariable String email) {
+		System.out.println("metodo checkEmail");
+		System.out.println(email);
+
+		return repo.findByEmail(email);
+		
+	}
+
 }
--- src/main/java/forumService/forumService/repository/userRepository.java
+++ src/main/java/forumService/forumService/repository/userRepository.java
@@ -1,9 +1,11 @@
 package forumService.forumService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import forumService.forumService.models.user;
 
 public interface userRepository extends JpaRepository<user, Long> {
-
+	@Query("select u from user u where u.email = ?1")
+	user findByEmail(String email);
 }
